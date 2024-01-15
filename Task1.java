cfba14aadaa8788bea7f28ebd31ccb9ecab436c8
VaibhaviZamde <155444140+VaibhaviZamde@users.noreply.github.com>
2024-01-15 21:17:59 +0530

    Task 1 is completed+

---
+++ Task1.java
@@ -0,0 +1,82 @@
+import java.util.*;
+class Task1
+{
+	public static void main(String args[])
+	{
+	
+		
+		System.out.println();
+		System.out.println("7  8  9  d  C\n");
+		System.out.println("4  5  6  x  /\n");
+		System.out.println("1  2  3  +  -\n");
+		System.out.println("0  .   ");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		/*System.out.println();
+		int a = scan.nextInt();
+		
+		System.out.println();
+		int b = scan.nextInt();
+		
+		int c = a+b;
+		System.out.println(c);
+		*/
+		
+		char choice ;
+		
+		do 
+		{
+			System.out.println();
+			int a= scan.nextInt();
+			System.out.println();
+			choice = scan.next().charAt(0);
+			System.out.println();
+			int b=scan.nextInt();
+			
+			switch(choice)
+			{
+				case '+':
+					System.out.println(a+b);
+					break;
+				
+				default:
+					break;
+			}
+			
+		}
+		while(choice!='C');
+	}
+}
+	case '-':
+		System.out.println("Result: " + (a - b));
+		break;
+	case '*':
+		System.out.println("Result: " + (a * b));
+		break;
+	case '/':
+		if (b != 0) 
+		{
+		 	System.out.println("Result: " + ((double) a / b));
+		} 
+		else 
+		{
+            		System.out.println("Error: Cannot divide by zero.");
+        	}
+       		break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
