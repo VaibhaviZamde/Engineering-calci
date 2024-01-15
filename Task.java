ad8878c5a6a2ce48a7f243e93d77e32e207eaa33
VaibhaviZamde <155444140+VaibhaviZamde@users.noreply.github.com>
2024-01-15 21:20:40 +0530

    Displaying the calculator on terminal

---
+++ Task.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Task
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		int x,y;
+		
+		System.out.println();
+		System.out.println("7  8  9  d  AC\n");
+		System.out.println("4  5  6  x  /\n");
+		System.out.println("1  2  3  +  -\n");
+		System.out.println("0  .   \n");
+		
+				
+		
+	}
+}
